a78585ea8ce217f2c35dd69871a1893e97f02efe
Progyan Bhattacharya <progyanb@acm.org>
2018-12-01 15:43:39 +0530

    Network Security: Header Sniffing and CSRF Protection
    Provided cookie support as well.
    
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 /** Import CommonJS Modules and Router */
-const express     = require("express"),
-      bodyParser  = require("body-parser"),
-      morgan      = require("morgan"),
-      path        = require("path"),
-      serveStatic = require("serve-static"),
-      config      = require("./Configs/appconfig"),
-      routes      = require("./Routes");
+const express      = require("express"),
+      bodyParser   = require("body-parser"),
+      cookieParser = require("cookie-parser"),
+      csrf         = require("csurf"),
+      errorHandler = require("errorhandler"),
+      helmet       = require("helmet"),
+      morgan       = require("morgan"),
+      path         = require("path"),
+      serveStatic  = require("serve-static"),
+      config       = require("./Configs/appconfig"),
+      routes       = require("./Routes");
 
 /** Instantiate an Express Application */
 const app = express();
@@ -13,10 +17,17 @@ const app = express();
 /** Inject Middlewares to Application */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(csrf({ cookie: true }));
+app.use(errorHandler());
+app.use(helmet());
 app.use(morgan("tiny"));
 app.use(serveStatic(path.join(__dirname, "Static")));
 app.set("view engine", "pug");
 app.set("views", "Views");
+
+/** Set Application Locale */
+app.locals.pretty = true;
 
 /** Define Route Paths and Parameters */
 app.get("/", routes.index);
